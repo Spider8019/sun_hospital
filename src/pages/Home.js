9f62c3d18011a79d9c73dae137c99d8085e988eb
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2023-12-31 13:37:57 +0530

    happy moments

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,7 @@ import YourDoctors from '../sections/YourDoctors'
 import PatientStories from '../sections/PatientStories'
 import Services from '../sections/Services'
 import AboutUs from '../sections/AboutUs'
+import HappyMoments from '../sections/HappyMoments'
 
 const Home = () => {
   const { t } = useTranslation()
@@ -16,7 +17,7 @@ const Home = () => {
         className="p-8 sm:p-20 bg-center bg-cover backdrop-blur-3xl grid place-items-center"
         style={{
           backgroundImage:
-            "url('https://images.pexels.com/photos/236380/pexels-photo-236380.jpeg?auto=compress&cs=tinysrgb&w=800')",
+            "url('https://i.pinimg.com/originals/8c/7e/9d/8c7e9d2e3ca8794be10076a5d1086c06.png')",
         }}
       >
         <div className="bg-[#ffffffbc] text-center p-12 rounded-xl">
@@ -45,8 +46,11 @@ const Home = () => {
       <div className="p-8 sm:p-20">
         <PatientStories />
       </div>
-      <div className="p-8 sm:px-20 sm:mb-40">
+      <div className="p-8 sm:p-20">
         <YourDoctors />
+      </div>
+      <div className="p-8 sm:px-20 sm:mb-40">
+        <HappyMoments />
       </div>
     </div>
   )
---
+++ src/sections/HappyMoments.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+const HappyMoments = () => {
+  const { t } = useTranslation()
+  return (
+    <div className="">
+      <div>
+        <p className="text-4xl font-semibold">
+          {t('home.happy_moments.heading_p1')}{' '}
+          <span className="text-baseColor">
+            {t('home.happy_moments.heading_p2')}
+          </span>
+        </p>{' '}
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 sm:gap-8">
+        <div className="w-full h-60">
+          <img
+            className="w-full h-full object-cover block rounded-xl shadow"
+            src="https://i.pinimg.com/originals/43/ed/5b/43ed5b114551f4b9b3ae38260d4fb772.png"
+            alt="Moments 1"
+          />
+        </div>
+        <div className="w-full h-60">
+          <img
+            className="w-full h-full object-cover block rounded-xl shadow "
+            src="https://i.pinimg.com/originals/3c/0f/3d/3c0f3d052f77dcfb1dc6b9f65794210b.png"
+            alt="Moments 1"
+          />
+        </div>
+        <div className="w-full h-60">
+          <img
+            className="w-full h-full object-cover block rounded-xl shadow "
+            src="https://i.pinimg.com/originals/6a/a3/70/6aa37014a490f2ca1606b15d8843a1d2.jpg"
+            alt="Moments 1"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HappyMoments
